fef7fd561d9e5ea79b48f61bddd4634f67e224c1
LTW <ltwsamuel@outlook.com>
2020-05-17 22:48:13 +0800

    Added comments for the api files.

--- api/taskupdate.py
+++ api/taskupdate.py
@@ -1,10 +1,29 @@
+# -*- coding: utf-8 -*-
+'''
+@author LTW
+@desc This is the api for use in your program to update task information to a taskMonitor Server.
 
+function: updateTask(),generateTaskID(),getTaskID()
+data:__taskID
+'''
 import socket
 import json
 import time
 import random
 
 def updateTask(taskName,taskProgress,taskInfo,taskid,taskServer=('127.0.0.1',4200)):
+    '''Update your task to the server.
+
+    Sends an UDP message to the server with task informations.
+
+    @param taskName: A str, the name for your program task.
+    @param taskProgress: A number(int or float), the progress of your task.
+    @param taskInfo: A str, the detailed information about your task's current progress.
+    @param taskid: An object of any serializable type in JSON, this marks your task. And therefore you
+        should **use a consistent one** during the program. To avoid collide, you can use the generate
+        method below.
+    @param taskServer: A tuple of (str,int), the server ip information in python socket format.
+    '''
     with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as sock:
         datadict={"name":taskName,"progress":taskProgress,"info":taskInfo,"taskid":taskid}
         jsonStr=json.dumps(datadict)
@@ -12,9 +31,8 @@ def updateTask(taskName,taskProgress,taskInfo,taskid,taskServer=('127.0.0.1',420
         sock.sendto(data,taskServer)
        
 def generateTaskID():
+    '''Generate a task id that will hardly collide.
+
+    @return A taskID str.
+    '''
     return hex(int(time.time()))[2:]+hex(random.randint(0,0xffffff))[2:]
-
-__taskID=generateTaskID()
-
-def getTaskID():
-    return __taskID
--- api/taskupdate.rb
+++ api/taskupdate.rb
@@ -1,7 +1,23 @@
+#-*- coding: UTF-8 -*-
+#Author:LTW
+#This is the api for use in your program to update task information to a taskMonitor Server.
 require 'socket'
 require 'json'
 require 'date'
 
+=begin
+Update your task to the server.
+
+Sends an UDP message to the server with task informations.
+
+@param taskName: A str, the name for your program task.
+@param taskProgress: A number(int or float), the progress of your task.
+@param taskInfo: A str, the detailed information about your task's current progress.
+@param taskid: An object of any serializable type in JSON, this marks your task. And therefore you
+    should **use a consistent one** during the program. To avoid collide, you can use the generate
+    method below.
+@param taskServer: A list of [ip address,port].
+=end
 def updateTask (taskName,taskProgress,taskInfo,taskid,taskServer=['127.0.0.1',4200])
     taskDict={
         "name"=>taskName,
@@ -13,6 +29,11 @@ def updateTask (taskName,taskProgress,taskInfo,taskid,taskServer=['127.0.0.1',42
     sock.send(JSON.generate(taskDict),0,taskServer[0],taskServer[1])
 end
 
+=begin
+Generate a task id that will hardly collide.
+
+@return A taskID str.
+=end
 def generateTaskID
     return ((Time.now.to_datetime.strftime '%Q').to_i()/1000).to_s(16)+rand(0xffffff).to_s(16)
 end
