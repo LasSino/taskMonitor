322aa0f66e2b8e8ebeff5491957b55ddfa326d6b
LTW <ltwsamuel@outlook.com>
2020-05-17 23:31:53 +0800

    The program exits even more gracefully using ctrl-c.

--- server/main.py
+++ server/main.py
@@ -6,8 +6,11 @@ from httphandler import HttpHandler
 from udplistener import UdpListener
 import httpthread
 
+app=None
+
 def ctrlchandler(signum,_):
     reactor.stop()
+    app.stop()
 
 if __name__ == '__main__':
     signal.signal(signal.SIGTERM,ctrlchandler)
@@ -15,7 +18,7 @@ if __name__ == '__main__':
     urls = ("/tasks", "HttpHandler")
     app = web.application(urls,globals())
     t1=httpthread.HTTPThread(app)
-    t1.setDaemon(True)
+    #t1.setDaemon(True)
     t1.start()
     reactor.listenUDP(4200,UdpListener())
     reactor.run()
