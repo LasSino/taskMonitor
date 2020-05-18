832700add9f513ef09393c97f1274bcd4e9425c2
LTW <ltwsamuel@outlook.com>
2020-05-18 14:40:30 +0800

    Moved refreshTaskList to tasklist.py.

--- server/tasklist.py
+++ server/tasklist.py
@@ -1,2 +1,29 @@
+# -*- coding: utf-8 -*-
+from time import time
+
+'''A global var. The task list.
+'''
 tasks={
 }
+
+def refreshTaskList():
+    '''This refershes the task list.
+
+    This function removes the finished and outdated tasks from the task list.
+    It also removed the seemingly aborted task from the list. 
+    This applies to the tasks that are not updated for a week.
+
+    As you can see, this is passively called when new updates are made to the tasks.
+    '''
+    remove=[]
+    for task in tasks.values():
+        if int(time())-task["time"]>=604800:
+            remove.append(task["taskid"])
+        if task["progress"]>=100:
+            if "time" in task:
+                if int(time())-task["time"]>=3600:
+                    remove.append(task["taskid"])
+            else:
+                remove.append(task["taskid"])
+    for toRemove in remove:
+        del tasks[toRemove]
--- server/udplistener.py
+++ server/udplistener.py
@@ -1,22 +1,8 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
-from tasklist import tasks
+from tasklist import tasks,refreshTaskList
 import json
 from time import time
-
-def refreshTaskList():
-    remove=[]
-    for task in tasks.values():
-        if int(time())-task["time"]>=604800:
-            remove.append(task["taskid"])
-        if task["progress"]>=100:
-            if "time" in task:
-                if int(time())-task["time"]>=3600:
-                    remove.append(task["taskid"])
-            else:
-                remove.append(task["taskid"])
-    for toRemove in remove:
-        del tasks[toRemove]
 
 class UdpListener(DatagramProtocol):
     def datagramReceived(self, datagram, addr):
